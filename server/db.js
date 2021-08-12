ed9acdb58be3b67067c2c51236d95541b942aa2c
vandvassily <zhs25427199@gmail.com>
2021-08-13 02:04:29 +0800

    feat: 服务端添加根据hash复制图片

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const { writeFile } = require('./file');
 
-const hashName = "asfdasdasd";
+const hashName = 'aabbcc';
+const dbPath = path.join(__dirname, 'db.json');
 
 function readDB() {
-    return JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+  return JSON.parse(fs.readFileSync(dbPath));
 }
 
 function isFileMatched(hashName) {
@@ -12,14 +14,36 @@ function isFileMatched(hashName) {
   return !!db[hashName];
 }
 
-console.log(isFileMatched(hashName));
+function insertToDB(hashName, file) {
+  const db = readDB();
+  const { filePath, userId } = file;
+  db.map[hashName] = {
+    filePath,
+    userId,
+  };
+
+  fs.writeFileSync(dbPath, JSON.stringify(db));
+}
 
 // TODO: 待完成
-// function copyFile(hashName, serverPath) {
-//     // 1. 读取数据库
-//     // 2. 复制第一条数据
-//     const db = readDB().map;
-//     const fileList = db[hashName]
+async function copyFile(hashName, serverPath) {
+  // 1. 读取数据库
+  // 2. 复制第一条数据
+  const db = readDB().map;
+  const fileList = db[hashName];
 
-//     const 
-// }
+  const file = fileList[0];
+  await writeFile(file.filePath, serverPath);
+}
+
+
+console.log(isFileMatched(hashName));
+
+if(isFileMatched(hashName)) {
+  const userId = 'vassily';
+  const dirPath = path.join(__dirname,'../upload', userId);
+  if(!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
+  }
+  copyFile(hashName, path.join(dirPath, 'logo.jpg'))
+}
